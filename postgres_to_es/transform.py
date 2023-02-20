4818619534b3767485a4365edb0d97aab5cc53c7
liza.Zaharuk <liza.zakharyuk@mail.ru>
2023-02-20 16:37:59 +0400

    Change transform_data method

--- postgres_to_es/transform.py
+++ postgres_to_es/transform.py
@@ -1,35 +1,31 @@
-def add_dict(result_list: list, item: dict, current_id: int):
-    """Функция добавления словаря в результирующий список по current_id"""
-    result_list.append(dict())
-    result_list[current_id]['id'] = item['fw_id']
-    result_list[current_id]['title'] = item['title']
-    result_list[current_id]['description'] = item['description']
-    result_list[current_id]['imdb_rating'] = item['rating']
-    result_list[current_id]['actors'] = []
-    result_list[current_id]['actors_names'] = []
-    result_list[current_id]['writers'] = []
-    result_list[current_id]['writers_names'] = []
-    result_list[current_id]['genre'] = []
+def create_dict(item: dict) -> dict:
+    """Функция """
+    temp_dict = dict()
+    temp_dict['id'] = item['fw_id']
+    temp_dict['title'] = item['title']
+    temp_dict['description'] = item['description']
+    temp_dict['imdb_rating'] = item['rating']
+    temp_dict['actors'] = []
+    temp_dict['actors_names'] = []
+    temp_dict['writers'] = []
+    temp_dict['writers_names'] = []
+    temp_dict['genres'] = []
+    return temp_dict
 
 
-def add_info_to_dict(result_list: list, item: dict, current_id: int):
-    """Функция добавления  информации в словарь результирующего списка по current_id"""
-    if item['g_name'] not in result_list[current_id]['genre']:
-        result_list[current_id]['genre'].append(item['g_name'])
-    if item['role'] == 'director':
-        result_list[current_id]['director'] = item['full_name']
-    elif item['role'] == 'actor':
-        actor_dict = {'id': item['p_id'], 'name': item['full_name']}
-        if actor_dict not in result_list[current_id]['actors']:
-            result_list[current_id]['actors'].append(actor_dict)
-        if item['full_name'] not in result_list[current_id]['actors_names']:
-            result_list[current_id]['actors_names'].append(item['full_name'])
-    elif item['role'] == 'writer':
-        writer_dict = {'id': item['p_id'], 'name': item['full_name']}
-        if writer_dict not in result_list[current_id]['writers']:
-            result_list[current_id]['writers'].append(writer_dict)
-        if item['full_name'] not in result_list[current_id]['writers_names']:
-            result_list[current_id]['writers_names'].append(item['full_name'])
+def add_info_to_dict(item: dict, temp_dict: dict):
+    """Функция d"""
+    for p in item['persons']:
+        if p['person_role'] == 'director':
+            temp_dict['director'] = p['person_name']
+        elif p['person_role'] == 'actor':
+            temp_dict['actors_names'].append(p['person_name'])
+            temp_dict['actors'].append({"id": p['person_id'], "name": p['person_name']})
+        elif p['person_role'] == 'writer':
+            temp_dict['writers_names'].append(p['person_name'])
+            temp_dict['writers'].append({"id": p['person_id'], "name": p['person_name']})
+    for g in item['genres']:
+        temp_dict['genres'].append(g)
 
 
 class DataTransformer:
@@ -38,26 +34,10 @@ class DataTransformer:
         """Функция приводит данные в формат, соответствующий схеме индекса в ElasticSearch
             Args: генератор словарей
         """
-        list_data = [item for item in data]
-        list_data.sort(key=lambda dictionary: dictionary['fw_id'])
-        result_list = []
-        current_id = 0
-        for item in list_data:
-            if not result_list or result_list[current_id]['id'] != item['fw_id']:
-                if result_list:
-                    current_id += 1
-                add_dict(result_list, item, current_id)
-                add_info_to_dict(result_list, item, current_id)
-            else:
-                add_info_to_dict(result_list, item, current_id)
-
-        return result_list
-
-    def gen_json_data(self, data):
-        """Функция генерирует из списка данные для загрузки в ElasticSearch"""
-        list_data = self.transform_data(data)
-        for item in list_data:
+        for item in data:
+            temp_dict = create_dict(item)
+            add_info_to_dict(item, temp_dict)
             yield {
                 "_index": "movies",
-                "_id": item['id'],
-                "_source": item}
+                "_id": item['fw_id'],
+                "_source": temp_dict}
