f229dbd72233322831d97d63d9b44e70df966502
liza.Zaharuk <liza.zakharyuk@mail.ru>
2023-02-20 17:00:40 +0400

    Hide ElasticSearch

--- postgres_to_es/load.py
+++ postgres_to_es/load.py
@@ -1,10 +1,12 @@
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import bulk
 
-es = Elasticsearch('http://127.0.0.1:9200')
-
 
 class ElasticLoader:
+
+    def __init__(self, es: Elasticsearch):
+        self.es = es
+
     def load(self, data):
         """"Принимает генератор словарей и пачками загружает данные в Elasticsearch"""
-        bulk(es, data)
+        bulk(self.es, data)
